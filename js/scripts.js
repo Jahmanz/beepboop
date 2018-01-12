5323c185d50eff9150fa587ce0d6fc47a1c496bc
Jahmanz Williams <savojah@yahoo.com>
2018-01-12 11:59:55 -0800

    adds html and css codes

---
+++ js/scripts.js
@@ -0,0 +1,83 @@
+$(document).ready(function() {
+  $("#frontback").submit(function(event) {
+    $("#frontback").show("#CSS");
+      var frontbackInput = $("input#frontback")
+      // if (frontback === "frontend") {
+        $(#CSS).show()
+      }
+  });
+});
+
+
+
+
+
+
+
+
+// $(document).ready(function() {
+//   $("form#insurance").submit(function(event) {
+//     var age = parseInt($("input#age").val());
+//     var gender = $("select#gender").val();
+// //
+//     if (age) {
+//       var quote = (100 - age) * 3;
+//       if (gender === 'male' && age < 26) {
+//         quote += 50;
+//       }
+//
+//       $("#rate").empty().append(quote);
+//       $("#quote").show();
+//     } else {
+//       alert('Please enter your age.');
+//     }
+//
+//     event.preventDefault();
+//   });
+// });
+
+// $(document).ready(function() {
+//     $("form#frontback").submit(function() {
+//       if (frontback === "frontend") {
+//           $("#CSS").show();
+//           $("#Csharp").hide();
+//           $("#Java").hide();
+//           $("#PHP").hide();
+//           $("#Ruby").hide();
+//
+//   // if (frontback === "CSS") {
+//   //   $('#CSS').show();
+//   //   $("#Csharp").hide();
+//   //   $("#Java").hide();
+//   //   $("#PHP").hide();
+//   //   $("#Ruby").hide();
+//   //
+//   // if (frontback === "CSS") {
+//   //   $('#CSS').show();
+//   //   $("#Csharp").hide();
+//   //   $("#Java").hide();
+//   //   $("#PHP").hide();
+//   //   $("#Ruby").hide();
+//   //
+//   // if (frontback === "CSS") {
+//   //   $('#CSS').show();
+//   //   $("#Csharp").hide();
+//   //   $("#Java").hide();
+//   //   $("#PHP").hide();
+//   //   $("#Ruby").hide();
+//   //
+//   // if (frontback === "CSS") {
+//   //   $('#CSS').show();
+//   //   $("#Csharp").hide();
+//   //   $("#Java").hide();
+//   //   $("#PHP").hide();
+//   //   $("#Ruby").hide();
+//   // }
+//   // if (animal === "cats") {
+//   //   $('#cats').show();
+//   // }
+//   // if (animal === "honeybadger") {
+//   //   $('#honeybadger').show();
+//   // }
+//
+// });
