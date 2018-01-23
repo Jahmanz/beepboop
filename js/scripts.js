b31c5e297862a007455d1179cde94353b6743dd3
Jahmanz Williams <savojah@yahoo.com>
2018-01-22 16:28:38 -0800

    adds js and html elements to project, along with revamp biz and user logic

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,30 @@
 
+var beepBoop = function(number) {
+  if (number === 0){
+    return true;
+  }
+  if (number === 1){
+    return false;
+  } else {
+    return alert("Im Sorry I can't do that dave")
+  }
+};
 
-$(document).ready(function () {
-  $("form#formTracker").submit(function (event) {
+// user interface logic
+$(document).ready(function() {
+  $("form#beepboopForm").submit(function(event) {
     event.preventDefault();
+    var number = parseInt($("input#userInput").val());
+    var result = beepBoop(number);
 
-    if (frontback === "front-end")
-      $("#courses").submit();
-      $(".answerCSS").show("#CSS");
+    // $(".year").text(number);
+
+    if (!result) {                 // same as writing if (result === false)
+      $(".beep").text("beep");
+    } if (result === true) {
+      $(".boop").text("boop");
+    }
+
+    $("#result").show();
   });
 });
