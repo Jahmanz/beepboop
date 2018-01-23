2b554e0a91f64f62b084a76c7cb31b7975aa6632
Jahmanz Williams <savojah@yahoo.com>
2018-01-23 12:59:20 -0800

    adds more html and css elements

--- js/scripts.js
+++ js/scripts.js
@@ -4,25 +4,25 @@ var beepBoop = function(number) {
   var stringNumber = number.toString();
 
   if (stringNumber.includes("0")) {
-    return true;
-  } else if (stringNumber.includes("1")){
     return false;
+  } else if (stringNumber.includes("1")){
+    return true;
 
   } else {
     return alert("I'm sorry I can't do that puny human").hide()
   }
 };
 
-// user interface logic
+
 $(document).ready(function() {
   $("form#beepboopForm").submit(function(event) {
     event.preventDefault();
     var number = parseInt($("input#userInput").val());
     var result = beepBoop(number);
 
-    // $(".year").text(number);
 
-    if (!result) {                 // same as writing if (result === false)
+
+    if (!result) {
       $(".beep").append(" beep");
     } if (result === true) {
       $(".boop").append(" boop");
